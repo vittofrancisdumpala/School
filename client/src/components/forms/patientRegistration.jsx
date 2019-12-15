73a7ebc72300facd3f5d5b3e5348ae6baba4cb2e
Prakash Dathu Parsa <parsadathu@gmail.com>
2019-12-15 18:31:20 +0530

    Fix fileNot Found issue

--- client/src/components/forms/patientRegistration.jsx
+++ client/src/components/forms/patientRegistration.jsx
@@ -244,7 +244,6 @@ function PatientRegistration(props) {
                                     </Button>
                                 </GridItem>
                             </GridContainer>
-                                { <DisplayFormikState {...props} />}
                         </form>
                     );
                 }}
@@ -265,20 +264,7 @@ const mapDispatchToProps = dispatch => ({
         dispatch(createPatient(values));
     }
 });
-export const DisplayFormikState = props => (
-    <div style={{ margin: '1rem 0' }}>
-        <h3 style={{ fontFamily: 'monospace' }} />
-        <pre
-            style={{
-                background: '#f6f8fa',
-                fontSize: '.65rem',
-                padding: '.5rem',
-            }}
-        >
-      <strong>props</strong> = {JSON.stringify(props, null, 2)}
-    </pre>
-    </div>
-);
+
 export default connect(
     mapStateToProps,
     mapDispatchToProps
